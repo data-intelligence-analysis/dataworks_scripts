a37491f38619254066f99b7089f2aea812495474
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2020-01-15 20:23:14 -0600

    Add files via upload

---
+++ Correct_HeaderRows_v1.0/Header_Utility.py
@@ -0,0 +1,213 @@
+#Import replace library
+import re
+#Import os library
+import os
+#Import csv library
+import csv
+#Import String
+import string
+
+from collections import Counter
+import string
+import copy
+from typing import Optional, List
+
+SILLY_DELIMITERS = frozenset(string.ascii_letters + string.digits + '.')
+
+def same(sequence):
+    elements = iter(sequence)
+    try:
+        first = next(elements)
+    except StopIteration:
+        return True
+    for el in elements:
+        if el != first:
+            return False
+    return True
+
+def detect(text:str, default:Optional[str]=None, whitelist:Optional[List[str]]=None,
+blacklist:Optional[List[str]]=SILLY_DELIMITERS) -> Optional[str]:
+    r"""
+    Detects the delimiter used in text formats such as CSV and its
+    many counsins.
+
+    >>> detect(r"looks|like|the vertical bar\nis|the|delimiter\n")
+    '|'
+
+    `detect_delimiter.detect()` looks at the text provided to try to
+    find an uncommon delimiter, such as `' for whatever reason.
+
+    >>> detect('looks\x10like\x10something stupid\nis\x10the\x10delimiter')
+    '\x10'
+
+    When `detect()` doesn't know, it returns `None`:
+
+    >>> text = "not really any delimiters in here.\nthis is just text.\n"
+    >>> detect(text)
+
+    It's possible to provide a default, which will be used in that case:
+
+    >>> detect(text, default=',')
+    ','
+
+    By default, it will prevent avoid checking alpha-numeric characters
+    and the period/full stop character ("."). This can be adjusted via
+    the `blacklist` parameter.
+
+    If you believe that you know the delimiter, it's possible to provide
+    a list of possible delimiters to check for via the `whitelist` parameter.
+    If you don't provide a value, `[',', ';', ':', '|', '\t']` will be checked.
+    """
+    if whitelist:
+        candidates = whitelist
+    else:
+        candidates = list(',;:|\t')
+
+    sniffed_candidates = Counter()
+    likely_candidates = []
+
+    lines = []
+    # todo: support streaming
+    text_ = copy.copy(text)
+    while len(lines) < 5:
+        for line in text_.splitlines():
+            lines.append(line)
+
+    for c in candidates:
+        fields_for_candidate = []
+
+        for line in lines:
+            for char in line:
+                if char not in blacklist:
+                    sniffed_candidates[char] += 1
+            fields = line.split(c)
+            n_fields = len(fields)
+
+            # if the delimiter isn't present in the
+            # first line, it won't be present in the others
+            if n_fields == 1:
+                break
+            fields_for_candidate.append(n_fields)
+
+        if not fields_for_candidate:
+            continue
+
+        if same(fields_for_candidate):
+            likely_candidates.append(c)
+
+
+    # no delimiter found
+    if not likely_candidates:
+        if whitelist is None and sniffed_candidates:
+            new_whitelist = [char for (char, _count) in sniffed_candidates.most_common()]
+            return detect(text, whitelist=new_whitelist) or default
+        return default
+
+    if default in likely_candidates:
+        return default
+
+    return likely_candidates[0]
+
+with open('config.txt', 'r') as input: #config.txt contains directory (path) to folder
+    #read path
+    read = input.readline()
+    #read entire directory
+    index = read[read.index('/'):]
+    
+    #Using os library to read and open files in directory
+    for file in os.listdir(index):#("C:\\Users\\DO069840\\OneDrive - Cerner Corporation\\Desktop\\Correct_HeaderRows")
+        if file.endswith(".txt") and not file.startswith(("config","README","LICENSE-2.0")):
+            with open("header.txt","rt") as infile, open("header_repaired.txt", "wt") as outfile:
+                #Read Input File
+                reader = csv.reader(infile)
+                #Write to output file
+                writer = csv.writer(outfile)
+                #If delimiter is pipe
+                conversionComma = set('["/.!@#$%^&*[]()-+={}:<>?/\',`~]')
+                #If delimiter is Comma
+                conversionPipe = set('["/.!@#$%^&*[]()-+={}:<>?/\'|`~]')
+
+                #Execute Loop
+                for row in reader:
+                    
+                    delimiter = detect(str(row))
+
+                    #Condition for Delimiter
+                    if delimiter == '|':
+                        #Replace special characters
+                        new_row = ["".join('' if i in conversionComma else i for i in entry) for entry in row]
+                        writer.writerow(new_row)
+
+                    elif delimiter == ',':
+                        #Replace special characters
+                        new_row2 = ["".join('' if i in conversionPipe else i for i in entry) for entry in row]
+                        writer.writerow(new_row2)
+
+        elif file.endswith(".csv"):
+            with open("speciality.csv", "rt") as infile, open("repaired.csv", "wt") as outfile:
+
+                #Read Input File
+                reader = csv.reader(infile)
+
+                #Write to Output File
+                writer = csv.writer(outfile)
+
+                ####Special Characters
+                #If delmiter is pipe
+                conversionPipe = set('["/.!@#$%^&*[]()-+={}:<>?/\'|`~]')
+                #If delimiter is Comma
+                conversionComma = set('["/.!@#$%^&*[]()-+={}:<>?/\',`~]')
+
+                #Execute Loop
+                for row in reader:
+                    delimiter = detect(str(row))
+                    if delimiter == '|': ###Unable to "," fix problem if there is one occurrence of the character
+                        #Replace special characters
+                        newrow = ["".join('' if i in conversionComma else i for i in entry) for entry in row]
+                        #Write new header row in output file
+                        writer.writerow(newrow)
+                    elif delimiter == ',':
+                        #Replace special characters
+                        newrow2 = ["".join('' if i in conversionPipe else i for i in entry) for entry in row]
+                        #Write new header row in output file
+                        writer.writerow(newrow2)
+print("Successfully removed special characters")
+
+#Finding Duplicates
+with open('header_repaired.txt','rt') as infile, open('duplicate.txt','w') as outfile:
+    #readerComma = csv.reader(infile, delimiter=(",")) 
+    #readerPipe = csv.reader(infile, delimiter=("|"))
+    reader = csv.reader(infile)
+
+    for read in reader:
+        delimited = detect(str(read))
+        if delimited ==",":
+            duplicates =[i for i in read]
+            seen = set()
+            count = 0
+            for m in duplicates:
+                if m in seen:
+                    outfile.write(str(m.split(",")))
+                    count+=1
+                    continue
+                seen.add(m)
+            print("Duplicates found: ",count)
+            break
+        elif delimited =="|":
+            #for reader2 in read:
+                #duplicates_list.extend(reader2)
+            duplicates2 =[i for i in read]
+            seen = set()
+            string = duplicates2
+            deli = string[-1]
+            deli+="|"
+            stri =str(deli)
+            spl=stri.split('|')
+            count2 = 0
+            for m in spl:
+                if m in seen:
+                    outfile.write(str(m))
+                    count2+=1
+                seen.add(m)
+            print("Duplicates found: ",count2)        
+outfile.close()
